adbcbd595b7465eabffea8c25981fa90a85e55ac
John Jacob <jacobjohnrobert@gmail.com>
2018-07-04 00:17:37 -0500

    initial commit

---
+++ Image.js
@@ -0,0 +1,49 @@
+//a color is a number in the range [0, 256)
+//an image is an array of arrays of colors
+//a row is an array in an image
+//an image is an array of rows
+//rows is the number of rows in an image
+//columns is the number of colors in a row
+
+class Image {
+  constructor({matrix = [], rows = matrix.length, columns = matrix[0].length}) {
+    this.matrix = [];
+    for (let i = 0; i < rows; i++) {
+      let row = [];
+      for (let j = 0; j < columns; j++) {
+        if (matrix) {
+          row.push(new Color(matrix[i][j]));
+        } else {
+          row.push(new Color(41));
+        }
+      }
+      this.matrix.push(row);
+
+    }
+  }
+
+  render() {
+    const black = new Color(0);
+    this.matrix.forEach(row => {
+      let string = " ";
+      row.forEach(color => {
+        string += color.bashSequence() + " ";
+      });
+      console.log(string, black.bashSequence());
+    });
+  }
+
+}
+
+class Color {
+  constructor(c) { //c >= 0 && c <256
+    this.value = c;
+  }
+
+  bashSequence() {
+    const displayChar = "S"; //can become a variable to support text on top of background
+    return `\x1b[48;5;${this.value}m`
+  }
+}
+
+module.exports = Image;
---
+++ index.js
@@ -0,0 +1,18 @@
+const Image = require('./Image');
+
+let matrix = [];
+const columns = 40;
+const rows = 4;
+const interval = (231 - 16) / (columns * rows);
+for (let i = 0; i < rows; i++) {
+  let row = [];
+  for (let j = 0; j < columns; j++) {
+    row.push(Math.floor(16 + j * interval * (i + 1)));
+  }
+  matrix.push(row);
+}
+
+const image = new Image({
+  matrix: matrix
+});
+image.render();
